61f3a01221c6ac83f2b948fef0ec5b0e5125da48
spocklogic <averykarlin3@gmail.com>
2016-01-11 09:14:20 -0500

    Added client header

---
+++ client.h
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
