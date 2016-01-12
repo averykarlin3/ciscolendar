4e25e963bc1c9ef302590f582cc11007ce398701
hrxiao <hxiao7463@gmail.com>
2016-01-11 22:52:12 -0500

    calendar headers

---
+++ calendar.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
+
+int jd(char * greg) {
+
+}
+
+char * greg(int jd) {
+
+} 
+
+int dayoweek(char * greg) {
+
+}
+
+void display_month(char * input) {
+
+	//pulls reminder stuff
+}
+
+void display_day(char * input) {
+
+	//pulls reminder stuff
+}
+
+int main() {
+
+	printf("It's TIME for the Ciscolendar\n");
+
+
+
+	return 0;
+}
--- makefile
+++ makefile
@@ -5,6 +5,8 @@ server.o: server.c server.h
 	gcc server.c -c
 client.o: client.c client.h
 	gcc client.c -c
+calendar.o: calendar.c
+	gcc calendar.c -c
 runc: client
 	./client
 runs: server
